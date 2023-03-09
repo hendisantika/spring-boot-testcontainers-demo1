529555074708250c82b88f35449a23d82c43e479
hendisantika <hendisantika@yahoo.co.id>
2023-03-10 05:57:07 +0700

    #11 - Add NotificationQueryService ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/routing/NotificationQueryService.java
@@ -0,0 +1,18 @@
+package com.hendisantika.routing;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testcontainers-demo1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/10/23
+ * Time: 05:56
+ * To change this template use File | Settings | File Templates.
+ */
+public interface NotificationQueryService {
+
+    List<Notification> getAll();
+}
