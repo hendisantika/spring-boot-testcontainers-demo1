89aaeb70c0772b8aa0b17d2339ae7c1918f59173
hendisantika <hendisantika@yahoo.co.id>
2023-03-10 05:47:19 +0700

    #1 - Initial Commit ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/SpringBootTestcontainersDemo1Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestcontainersDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestcontainersDemo1Application.class, args);
+    }
+
+}
