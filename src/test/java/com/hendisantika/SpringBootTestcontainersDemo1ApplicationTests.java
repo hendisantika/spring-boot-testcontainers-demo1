0fc0311ccf2d04a69fbe8c0ae0b23139c2e98e8e
hendisantika <hendisantika@yahoo.co.id>
2023-03-10 06:02:58 +0700

    #19 - Add SpringBootTestcontainersDemo1ApplicationTests ✏️📗:octocat:

--- src/test/java/com/hendisantika/SpringBootTestcontainersDemo1ApplicationTests.java
+++ src/test/java/com/hendisantika/SpringBootTestcontainersDemo1ApplicationTests.java
@@ -2,9 +2,31 @@ package com.hendisantika;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class SpringBootTestcontainersDemo1ApplicationTests {
+    @Container
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest");
+
+    @Container
+    static GenericContainer activemq = new GenericContainer("rmohr/activemq").withExposedPorts(61616);
+
+    @Container
+    static GenericContainer rabbitmq = new GenericContainer("rabbitmq").withExposedPorts(5672);
+
+
+    @DynamicPropertySource
+    static void registerDynamicProperties(DynamicPropertyRegistry registry) {
+        DemoApplicationTestPropertyValues.populateRegistryFromContainers(registry, postgreSQLContainer, activemq,
+                rabbitmq);
+    }
 
     @Test
     void contextLoads() {
